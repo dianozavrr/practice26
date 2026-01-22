1c1606828284f1f6ddec7a452fdd8d05381f280c
dianozavrr <levchenkodiana6852@gmail.com>
2026-01-22 02:11:09 +0200

    Add week1 code

---
+++ 1.1.py
@@ -0,0 +1,23 @@
+import this
+
+a = 10
+b = 3
+c = 2.5
+d = 1.5
+s = "привіт"
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print(c + d)
+print(c - d)
+print(c * d)
+print(c / d)
+
+print(s)
+
+n = input("Name:")
+
+print(s + + n)
