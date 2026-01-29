c55b71c39665c3cb636889d1df5f94533b42c2e3
dianozavrr <levchenkodiana6852@gmail.com>
2026-01-29 19:56:10 +0200

    .

---
+++ WEEK2/2.8.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, r):
+        self.r = r
+    def area(self):
+        return 3.14 * self.r * self.r
+
+class Rectangle(Shape):
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+    def area(self):
+        return self.w * self.h
+
+c = Circle(3)
+r = Rectangle(4, 5)
+
+print(c.area())
+print(r.area())
